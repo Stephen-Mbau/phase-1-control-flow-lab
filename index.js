fd963b070537539c9909ed98539407b4360da7be
Stephen Mbau <mbaustephen@gmail.com>
2023-07-05 09:42:37 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(string) {
   // Write your code here!
+  if(string <= 199) {return "This one is on me!"}
+  else if(string===2001) {return "I will gladly take your thirty bucks."}
+  else if(string===2501) {return "No can do."}; 
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(n){
   // Write your code here!
+  return n === "NYC" ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(Tip){
   // Write your code here!
+    switch(Tip) {
+      case 'generous':
+        return 'Thank you so much.';
+      case 'not as generous':
+        return 'Thank you.';
+      case 'thanks for everything':
+        return 'Bye.';
+    }
+
 }
